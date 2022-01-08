f17fb2e1dca2a6ef994d542da4dbc1c9888f4f43
t.ismoilov <t.ismoilov@paymo.uz>
2022-01-08 15:15:50 +0500

    Initial commit

---
+++ src/main/java/guru/springframework/jmsapp/JmsAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/jmsapp/JmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
